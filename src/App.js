9d02857ae79abe9ca68fd14b5533a3631be78310
Shail_Office <shailshah.officework@gmail.com>
2022-08-18 17:00:51 +0530

    Implemented pagination module on pagination route --office

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/UI/Navbar";
 import InvoiceForm from "./components/pages/InvoiceForm";
 import InvoiceDetail from "./components/pages/InvoiceDetails";
+import UserInfo from "./components/pagination/UserInfo";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<InvoiceForm />}></Route>
         <Route path="/invoicedetail" element={<InvoiceDetail />}></Route>
+        <Route path="/userinfo" element={<UserInfo/>}></Route>
       </Routes>
     </>
   );
